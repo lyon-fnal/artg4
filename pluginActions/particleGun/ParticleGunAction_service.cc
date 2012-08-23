3b88f6275ee6ed03a6e5e1f73d5a30fd947efe25
Natasha Arvanitis <imsa1010@gm2gpvm01.fnal.gov>
2012-08-23 09:27:49 -0500

    Ported particleGun action to service setup. Not quite ready to run yet.

---
+++ pluginActions/particleGun/ParticleGunAction_service.cc
@@ -0,0 +1,46 @@
+// This file provides the implementation for an action object that produces
+// the primary particles for the simulation.
+
+// Authors: Tasha Arvanitis, Adam Lyon
+// Date: August 2012
+
+#include "artg4/pluginActions/particleGun/ParticleGunAction_service.hh"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "artg4/services/ActionHolder_service.hh"
+
+#include <iostream>
+
+using std::string;
+
+artg4::ParticleGunActionService::
+ParticleGunActionService(fhicl::ParameterSet const & p, 
+			 art::ActivityRegistry &)
+  : PrimaryGeneratorActionBase(p.get<string>("name","particleGun")),
+    // Initialize our general particle source using a sub-pset from the passed
+    // parameters.
+    GPS_(new GeneralParticleSource(p.get<fhicl::ParameterSet>("GPS"))),
+    // Initialize our message logger
+    logInfo_("ParticleGunAction")
+{
+  // Register ourselves with the ActionHolder
+  art::ServiceHandle<ActionHolderService> actionHolder;
+
+  actionHolder -> registerAction(this);
+}
+
+// Destructor
+artg4::ParticleGunActionService::~ParticleGunActionService()
+{
+  delete GPS_;
+}
+
+// Create a primary particle for an event!
+// (Standard Art G4 simulation)
+void artg4::ParticleGunActionService::GeneratePrimaries(G4Event * anEvent)
+{
+  // Everything is taken care of by the general particle source!
+  GPS_ -> GeneratePrimaryVertex(anEvent);
+}
+
+using artg4::ParticleGunActionService;
+DEFINE_ART_SERVICE(ParticleGunActionService)
---
+++ pluginActions/particleGun/ParticleGunAction_service.hh
@@ -0,0 +1,61 @@
+// PrimaryGeneratorActionService is the service that injects particles into
+// the simulation.
+// To use this action, all you need to do is put it in the services section
+// of the configuration file, like this:
+// 
+// services: { 
+//   ...
+//   user: {
+//     PrimaryGeneratorActionService: {}
+//     ...
+//   }
+// }
+
+// Authors: Tasha Arvanitis, Adam Lyon
+// Date: August 2012
+
+// Include guard
+#ifndef PRIMARYGENERATORACTION_SERVICE_HH
+#define PRIMARYGENERATORACTION_SERVICE_HH
+
+// Includes
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "art/Framework/Core/EDProducer.h"
+
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "G4Event.hh"
+#include "G4ParticleGun.hh"
+#include "artg4/pluginActions/particleGun/GeneralParticleSource.hh"
+
+// Get the base class
+#include "actionBase/PrimaryGeneratorActionBase.hh"
+
+namespace artg4 {
+
+  class ParticleGunActionService : public PrimaryGeneratorActionBase {
+  public: 
+    ParticleGunActionService(fhicl::ParameterSet const&, 
+				  art::ActivityRegistry&);
+    virtual ~ParticleGunActionService();
+
+    // To generate primaries, we need to overload the GeneratePrimaries
+    // method. 
+    virtual void GeneratePrimaries(G4Event * anEvent);    
+
+    // We don't add anything to the event, so we don't need callArtProduces
+    // or FillEventWithArtHits.
+
+  private:
+    // Our particle source
+    GeneralParticleSource * GPS_;
+
+    // A message logger for this action
+    mf::LogInfo logInfo_;
+  };
+}
+
+#endif
