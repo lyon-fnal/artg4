dec2ed542469ffd02aeea52ee5022f65d89d5b8f
Adam Lyon <lyon@fnal.gov>
2012-11-05 12:49:30 -0600

    Make sure we are linking to geant libraries where we should

---
+++ services/PhysicsListHolder_service.h
@@ -0,0 +1,14 @@
+//
+//  PhysicsListHolder_service.h
+//  artg4example_xcode
+//
+//  Created by Adam Lyon on 11/5/12.
+//  Copyright (c) 2012 Adam Lyon. All rights reserved.
+//
+
+#ifndef __artg4example_xcode__PhysicsListHolder_service__
+#define __artg4example_xcode__PhysicsListHolder_service__
+
+#include <iostream>
+
+#endif /* defined(__artg4example_xcode__PhysicsListHolder_service__) */
