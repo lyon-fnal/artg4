a267df01a923f4e838a803b699956af5e59f3186
Brendan <kiburg@fnal.gov>
2012-11-27 18:22:02 -0600

    Simple fhicl settings interface. For implementing daughter structures that only access fhicl params, rather than creating a whole service to do that

---
+++ Core/RunSettingsBase.hh
@@ -0,0 +1,33 @@
+#ifndef RUNSETTINGS_BASE_HH
+#define RUNSETTINGS_BASE_HH
+
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "artg4/services/RunSettings_service.hh"
+
+
+#include "fhiclcpp/ParameterSet.h"
+
+#include <string>
+namespace artg4 {
+
+  struct RunSettingsBase { 
+    
+    RunSettingsBase(std::string const & listName) : 
+      name(listName),
+      p() 
+    {
+      art::ServiceHandle <artg4::RunSettingsService> s;
+      p = s->getSettings( listName );
+    }
+    
+    virtual ~RunSettingsBase() {}
+    
+    std::string name;
+    fhicl::ParameterSet p;
+    
+  };
+}
+
+
+#endif // RUNSETTINGS_BASE_HH
+
