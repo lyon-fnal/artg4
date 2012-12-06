38dd4279d7d06baee30bd90592f6498583ab594c
Brendan <kiburg@fnal.gov>
2012-12-05 18:06:11 -0600

    Added a templated helper function for grabbing sensitive detectors from the SDManager

--- util/util.hh
+++ util/util.hh
@@ -7,11 +7,22 @@
 #include <functional>
 #include <string>
 
+#include "Geant4/G4SDManager.hh"
+
 class G4LogicalVolume;
 class G4VisAttributes;
 
 namespace artg4 {
 
+  // class T should be a class that inherits from G4VSensitiveDetector
+  // but in principle this could should *do something* for any class that 
+  // has a constructor which takes a string
+  template <class T>
+  void getSensitiveDetector(G4String name, T* SDptr){
+    G4SDManager* fSDM = G4SDManager::GetSDMpointer();
+    if ((SDptr = (T*)(fSDM->FindSensitiveDetector(name))) == NULL)
+      SDptr = new T(name);
+  }
 
   // Set visual attributes
   void setVisAtts(G4LogicalVolume* lv, bool display, const std::vector<double> & rgba);
