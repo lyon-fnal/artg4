297c6f009f431a9d72f5d22cc1c3b18ea478229d
Brendan <kiburg@fnal.gov>
2012-12-07 14:36:41 -0600

    Improved the templated class to require G4VSensitiveDetectors and return a ptr

--- util/util.hh
+++ util/util.hh
@@ -14,15 +14,29 @@ class G4VisAttributes;
 
 namespace artg4 {
 
-  // class T should be a class that inherits from G4VSensitiveDetector
-  // but in principle this could should *do something* for any class that 
-  // has a constructor which takes a string
-  template <class T>
-  void getSensitiveDetector(G4String name, T* SDptr){
+  // This function either returns or creates an SD as necessary, based on 
+  // the string passed in. If the G4SDManager hasn't created an SD based on
+  // that string before, it will now.
+  // class T must be a class that inherits from G4VSensitive detector
+  //(that's what the 2nd line enforces). If it doesn't, then the templated
+  // class will not generate a method for you.
+
+  //To get a new RingSD, put something *like* this line in your service:
+  //RingSD* mysd = getSensitiveDetector<RingSD>("myRings");
+
+  template <typename T>
+  typename std::enable_if< std::is_base_of<G4VSensitiveDetector, T>::value, T* >::type
+  getSensitiveDetector(G4String name) {
+    T* SDptr=0;
     G4SDManager* fSDM = G4SDManager::GetSDMpointer();
-    if ((SDptr = (T*)(fSDM->FindSensitiveDetector(name))) == NULL)
+    if ((SDptr = static_cast<T*>(fSDM->FindSensitiveDetector(name))) == NULL)
       SDptr = new T(name);
+    return SDptr;
   }
+
+
+
+
 
   // Set visual attributes
   void setVisAtts(G4LogicalVolume* lv, bool display, const std::vector<double> & rgba);
